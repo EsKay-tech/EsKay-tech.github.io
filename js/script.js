d826e9c2e48553fd5b10d642c59478d6fed78d76
Sean Mohomane <karabo.mn33@gmail.com>
2020-11-30 16:44:56 +0200

    done

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 // Aside Navbar
 const nav=document.querySelector(".nav"),
-    navList=nav.querySelectorAll("li"),
-    totalNavList=navList.length,
-    allSection=document.querySelectorAll(".section"),
-    totalSection=allSection.length;
+      navList=nav.querySelectorAll("li"),
+      totalNavList=navList.length,
+      allSection=document.querySelectorAll(".section"),
+      totalSection=allSection.length;
 
 for(let i=0; i<totalNavList; i++){
     const a=navList[i].querySelector("a");
